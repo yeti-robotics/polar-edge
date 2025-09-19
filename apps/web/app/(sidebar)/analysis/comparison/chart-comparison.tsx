e4f80c9042c15df50af751a1f97421ac4eb6ba9d
AnirvinP <anirvin421potaraju@gmail.com>
2025-09-18 20:07:15 -0400

    Chart values added: still need to get the actual data from stand form into the fields, currently have test fields

--- apps/web/app/(sidebar)/analysis/comparison/chart-comparison.tsx
+++ apps/web/app/(sidebar)/analysis/comparison/chart-comparison.tsx
@@ -1,35 +1,45 @@
 "use client";
 import { type ChartConfig } from "@repo/ui/components/chart";
 import { ChartContainer } from "@repo/ui/components/chart";
-import { Bar, BarChart } from "recharts";
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
 
 const chartData = [
-	{ month: "January", desktop: 186, mobile: 80 },
-	{ month: "February", desktop: 305, mobile: 200 },
-	{ month: "March", desktop: 237, mobile: 120 },
-	{ month: "April", desktop: 73, mobile: 190 },
-	{ month: "May", desktop: 209, mobile: 130 },
-	{ month: "June", desktop: 214, mobile: 140 },
+    { level: "level1", game1: 5, game2: 7, game3: 3 },
+    { level: "level2", game1: 4, game2: 3, game3: 7 },
+    { level: "level3", game1: 6, game2: 4, game3: 5 },
+    { level: "level4", game1: 3, game2: 6, game3: 4 },
+    { total: "Total", game1: 18, game2: 20, game3: 19 },
+
 ];
 
-const chartConfig = {
-	desktop: {
-		label: "Desktop",
-		color: "#2563eb",
-	},
-	mobile: {
-		label: "Mobile",
-		color: "#60a5fa",
-	},
+const gameColors = {
+    game1: {
+        label: "game 1",
+        color: "#2563eb",
+    },
+    game2: {
+        label: "game 2",
+        color: "#60a5fa",
+    },
+    game3: {
+        label: "game 3",
+        color: "#3b82f6",
+    },
 } satisfies ChartConfig;
 
 export function ChartComparison() {
-	return (
-		<ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-			<BarChart accessibilityLayer data={chartData}>
-				<Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-				<Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
-			</BarChart>
-		</ChartContainer>
-	);
+    return (
+        <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={chartData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="level" />
+                <YAxis label={{ value: "Game number", angle: -90, position: "insideLeft" }} />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="game1" stackId="a" fill={gameColors.game1.color} name="Game 1" />
+                <Bar dataKey="game2" stackId="a" fill={gameColors.game2.color} name="Game 2" />
+                <Bar dataKey="game3" stackId="a" fill={gameColors.game3.color} name="Game 3" />
+            </BarChart>
+        </ResponsiveContainer>
+    );
 }
