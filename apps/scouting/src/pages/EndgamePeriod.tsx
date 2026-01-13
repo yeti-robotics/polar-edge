232ab74f6fa6696c3845a12afe2a9016bf9e05d9
AnirvinP <anirvin421potaraju@gmail.com>
2026-01-12 21:37:23 -0500

    Made a basic skeleton for standform (could not add in components because of Repo Nuke) (Will do this tomorrow)

---
+++ apps/scouting/src/pages/EndgamePeriod.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { useState } from "react";
+import { CardContent } from "@repo/ui/components/card";
+import { Checkbox } from "@repo/ui/components/checkbox";
+import { FormField, FormItem, FormLabel } from "@repo/ui/components/form";
+
+export function Engame() {
+    const [endgameData, setEndgameData] = useState()
+
+};
