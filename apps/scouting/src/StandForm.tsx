350b5b0081923b2bbcd29519c175474519520a4c
AnirvinP <anirvin421potaraju@gmail.com>
2026-01-16 18:57:10 -0500

    commiting very slight changes so that I can go work on the pitform on my computer and not shivangs

--- apps/scouting/src/StandForm.tsx
+++ apps/scouting/src/StandForm.tsx
@@ -1,11 +1,17 @@
 "use client";
 import React, { useState } from "react";
 
+<<<<<<< HEAD
 import {
     Auto,
     TeleopPeroid,
     Endgame
 } from './pages';
+=======
+import { Auto } from "./series/AutoPeriod";
+import { TeleopPeriod } from "./series/TeleopPeriod";
+import { Endgame } from "./series/EndgamePeriod";
+>>>>>>> b8a70c3 (commiting very slight changes so that I can go work on the pitform on my computer and not shivangs)
 
 // interface StandFormProps {
 //   onSubmit: (data: { standName: string; location: string }) => void;
@@ -20,7 +26,11 @@ const nextStep = (name?: string) => {
         case "auto":
             return <Auto />;
         case "teleop":
+<<<<<<< HEAD
             return <TeleopPeroid />;
+=======
+            return <TeleopPeriod />;
+>>>>>>> b8a70c3 (commiting very slight changes so that I can go work on the pitform on my computer and not shivangs)
         case "endgame":
             return <Endgame />;
         default:
