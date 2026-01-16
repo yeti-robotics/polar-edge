a65b1e00e03ea37040ff82bb4bf5d08838be0369
Shivang Patel <shivangpatel2050@gmail.com>
2026-01-15 19:55:11 -0500

    polished UI for pitform, plan to add sidebar in layout.tsx for the scouting form

--- apps/scouting/src/app/frontend/page.tsx
+++ apps/scouting/src/app/frontend/page.tsx
@@ -1,20 +1,28 @@
 "use client";
-
-import { useState } from "react";
 import PitForm from "@/components/PitForms";
+import { Card, CardContent, CardHeader, CardTitle } from "@repo/ui/components/card";
+import { FormField, FormItem, FormLabel} from "@repo/ui/components/form";
+import { Input } from "@repo/ui/components/input";
+import { Button } from "@repo/ui/components/button";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@repo/ui/components/select";
+import { useFormContext} from "react-hook-form";
 
-function FrontendOnlyForm() {
 
-    return(
+export default function FrontendOnlyForm() {
+  return (
+        <div className="flex justify-center p-4">
+      <Card className="w-full max-w-2xl">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Pit Scouting Form
+          </CardTitle>
+        </CardHeader>
 
-        <div>
-            <PitForm/>
-            
-                
+        <CardContent>
+          <PitForm />
+        </CardContent>
+      </Card>
+    </div>
 
-        </div>
-    )
+  );
 }
-
-export default FrontendOnlyForm;
-
