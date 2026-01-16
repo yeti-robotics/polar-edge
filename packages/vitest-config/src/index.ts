ad90ae6e81259368fa78aa82890a4be0df11d0e2
Drew Beamer <andrewmbeamer@gmail.com>
2026-01-15 21:23:46 -0500

    fix build of vitest-config

--- packages/vitest-config/src/index.ts
+++ packages/vitest-config/src/index.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vitest/config";
-import { baseConfig } from "./configs/base-config";
+import { baseConfig } from "./configs/base-config.js";
 
 export const sharedConfig = defineConfig({
   test: {
@@ -8,5 +8,5 @@ export const sharedConfig = defineConfig({
   },
 });
 
-export { baseConfig } from "./configs/base-config";
-export { uiConfig } from "./configs/ui-config";
+export { baseConfig } from "./configs/base-config.js";
+export { uiConfig } from "./configs/ui-config.js";
