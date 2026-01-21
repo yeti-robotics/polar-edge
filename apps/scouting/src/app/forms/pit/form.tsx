65f507a4818e67e711ba6218636df5e5f3e4ff3b
AnirvinP <anirvin421potaraju@gmail.com>
2026-01-20 20:05:47 -0500

    pitform PR

---
+++ apps/scouting/src/app/forms/pit/form.tsx
@@ -0,0 +1,74 @@
+import { Card } from "@repo/ui/components/card";
+import { Checkbox } from "@repo/ui/components/checkbox";
+import { Input } from "@repo/ui/components/input";
+import { Label } from "@repo/ui/components/label";
+
+export function PitForm() {
+  return (
+    <Card className="p-6">
+      <form className="space-y-6 ">
+        <section className="space-y-4">
+          <h2 className="text-lg text-neutral-200  ">Robot Dimensions</h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            <div className="flex flex-col gap-1">
+              <Label htmlFor="width">Width</Label>
+              <Input type="number" id="width" name="width" placeholder="Enter width" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Label htmlFor="length">Length</Label>
+              <Input type="number" id="length" name="length" placeholder="Enter length" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Label htmlFor="height">Height</Label>
+              <Input type="number" id="height" name="height" placeholder="Enter height" />
+            </div>
+
+            <div className="flex flex-col gap-1">
+              <Label htmlFor="weight">Weight</Label>
+              <Input type="number" id="weight" name="weight" placeholder="Enter weight" />
+            </div>
+          </div>
+        </section>
+
+        <section className="space-y-3">
+          <h2 className="text-lg text-neutral-200">Scoring Capabilities</h2>
+
+          <Input
+            type="text"
+            id="scoring-capabilities"
+            name="scoring-capabilities"
+            placeholder="Enter scoring capabilities"
+          />
+        </section>
+
+        <section className="space-y-3">
+          <h2 className="text-lg text-neutral-200">Climb Level</h2>
+
+          <div className="flex gap-6">
+            {["L1", "L2", "L3"].map((level) => (
+              <Label key={level} htmlFor={`climb-${level}`}>
+                <Checkbox id={`climb-${level}`} value={level} />
+                {level}
+              </Label>
+            ))}
+          </div>
+        </section>
+        <section className="space-y-3">
+          <h2 className="text-lg text-neutral-200">Robot Build</h2>
+
+          <div className="flex gap-6">
+            {["Bump", "Trench"].map((type) => (
+              <Label key={type}>
+                <input type="checkbox" name="build" value={type} />
+                {type}
+              </Label>
+            ))}
+          </div>
+        </section>
+      </form>
+    </Card>
+  );
+}
---
+++ apps/scouting/src/app/forms/pit/page.tsx
@@ -0,0 +1,10 @@
+import { PitForm } from "./form";
+
+export default function PitFormPage() {
+  return (
+    <main className="container mx-auto max-w-5xl space-y-4 py-8">
+      <h1 className="text-3xl tracking-tight "> Pit Form </h1>
+      <PitForm />
+    </main>
+  );
+}
