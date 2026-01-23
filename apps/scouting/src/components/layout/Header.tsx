787ba01fb5b15d273fac33f5157cb5039ac892c6
AnirvinP <anirvin421potaraju@gmail.com>
2026-01-22 19:43:18 -0500

    stickyNAV

--- apps/scouting/src/components/layout/Header.tsx
+++ apps/scouting/src/components/layout/Header.tsx
@@ -1,17 +1,7 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/components/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@repo/ui/components/dropdown-menu";
 import { headers } from "next/headers";
 import Link from "next/link";
-import { Suspense } from "react";
-import { auth } from "@/lib/auth";
+import { Suspense, Suspense } from "react";
+import { auth, auth } from "@/lib/auth";
 import { OrganizationSelector } from "./OrganizationSelector";
 
 async function AdminLink() {
@@ -26,41 +16,30 @@ async function AdminLink() {
   }
 }
 
-export async function Header() {
+//map through array to render the links
+
+export function Header() {
   return (
     <header className="sticky top-0 z-50 h-16 px-4 border-b flex items-center justify-between bg-background">
       <div className="flex items-center gap-4">
         <span className="font-mono uppercase text-xs">Polar Edge</span>
         <OrganizationSelector />
       </div>
-      <div className="ml-40 mt-3 flex justify-center items-center">
-        <nav className="flex items-center gap-4 text-xs font-mono text-muted-foreground mr-90">
-          <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/">
-            Home
-          </Link>
-          <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/auto-path">
-            Auto Paths
-          </Link>
-          <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/scouting">
-            Scouting
-          </Link>
-          <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/results">
-            Data
-          </Link>
-          <Link
-            className="flex items-center gap-4 text-xs font-mono text-muted-foreground mr-90"
-            href="/admin"
-          >
-            <AdminLink />
-          </Link>
-          <nav />
-          <Suspense>
-            <div className="size-9 ml-90 mt-1 h-12 flex-wrap justify-start">
-              <UserAvatar />
-            </div>
-          </Suspense>
-        </nav>
-      </div>
+
+      <nav className="flex items-center gap-4 text-xs font-mono text-muted-foreground mr-90 gap-10">
+        <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/">
+          Home
+        </Link>
+        <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/auto-path">
+          Auto Paths
+        </Link>
+        <Link className="hover:bg-gray-700 px-4 py-2 hover:text-white" href="/forms/pit">
+          Pit Form
+        </Link>
+        <Suspense>
+          <AdminLink />
+        </Suspense>
+      </nav>
     </header>
   );
 }
