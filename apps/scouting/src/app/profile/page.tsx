8ae0ca9ad30f7474a4bc3361608f0e1ba701c885
Shivang Patel <shivangpatel2050@gmail.com>
2026-01-24 16:39:09 -0500

    finished a few errors

--- apps/scouting/src/app/profile/page.tsx
+++ apps/scouting/src/app/profile/page.tsx
@@ -12,7 +12,7 @@ export default function ProfilePage() {
   if (isPending) {
     return (
       <div className="min-h-screen flex items-center justify-center bg-black text-sky-200">
-        <div className="text-center">Loading profile...</div>
+        <div className="text-center text-yeti-500">Loading profile...</div>
       </div>
     );
   }
@@ -31,7 +31,7 @@ export default function ProfilePage() {
   const user = session.user;
 
   return (
-    <main className="min-h-screen bg-black px-6 py-10">
+    <main className="min-h-screen bg-background-black px-6 py-10">
       <div className="max-w-5xl mx-auto">
         <header className="flex items-center gap-6 mb-8">
           <div className="w-24 h-24 rounded-full flex items-center justify-center overflow-hidden ">
@@ -39,12 +39,12 @@ export default function ProfilePage() {
               <Image
                 src={user.image}
                 alt={user.name ?? "avatar"}
-                className="w-full h-full object-cover border-transparent"
+                className="w-full h-full object-cover"
                 width={300}
                 height={300}
               />
             ) : (
-              <div className="text-black font-bold">
+              <div className="text-black font-mono">
                 {(user.name || "?").charAt(0)}
                 <h1> {user.name} </h1>
               </div>
@@ -53,29 +53,20 @@ export default function ProfilePage() {
           <Button onClick={handleLogOut}>Log Out</Button>
         </header>
 
-        <section className="bg-slate-900 rounded-lg p-6 ring-ring mb-8 size-full w-220 h-100 mt-10">
-          <h3 className="text-2xl font-semibold text-white-900 mb-3">Recent activity</h3>
-          <ul className="divide-y divide-slate-800">
+        <section className="bg-muted-black rounded-lg p-6 ring-ring mb-8 size-full w-220 h-100 mt-10">
+          <h3 className="text-2xl font-semibold text-foreground-white mb-3"> Recent activity</h3>
+          <ul>
             <li className="py-3 flex items-start justify-between">
               <div>
-                <div className="text-sm text-sky-100 font-medium"> Mock Data | Real Time Soon </div>
-              </div>
-            </li>
-            <li>
-              <div>
-                <div className="text-sm text-sky-100 font-medium"> YETI Team 3506 </div>
+                <div className="text-sm text-primary-white font-medium">
+                  Mock Data | Real Time Soon (Incorporate Drizzle or Forms that users have filled
+                  out)
+                </div>
               </div>
             </li>
           </ul>
         </section>
       </div>
-
-      <section className="rounded-lg p-6 ring-1 ring-slate-900 size-full w-220 h-100 mt-10">
-        <h1 className="text-2xl font-bold mb-4">Settings</h1>
-        <div className="flex flex-col gap-4"></div>
-
-        <Button>Save Settings</Button>
-      </section>
     </main>
   );
 }
