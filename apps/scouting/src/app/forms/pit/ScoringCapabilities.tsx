3f6112f83a21ffa34212200390e54edf120bec16
AnirvinP <anirvin421potaraju@gmail.com>
2026-01-24 10:24:26 -0500

    began working on incorporating 2 pages that can be moved back and forth for Pitform

--- apps/scouting/src/app/forms/pit/ScoringCapabilities.tsx
+++ apps/scouting/src/app/forms/pit/ScoringCapabilities.tsx
@@ -1,33 +1,19 @@
-import { Card } from "@repo/ui/components/card";
 import { Checkbox } from "@repo/ui/components/checkbox";
 import { Input } from "@repo/ui/components/input";
 import { Label } from "@repo/ui/components/label";
 
 export function ScoringCapabilities() {
   return (
-    <Card>
-      <form>
-        <section className="space-y-3">
-          <h2 className="text-lg text-neutral-200">Drivetrain Type</h2>
-          <div className="flex gap-6">
-            {["Tank", "Mecanum", "Swerve", "Other"].map((type) => (
-              <Label key={type} htmlFor={`drivetrain-${type}`}>
-                <Checkbox id={`drivetrain_${type}`} name={`drivetrain-${type}`} value={type} />
-                {type}
-              </Label>
-            ))}
-          </div>
-        </section>
-        <section className="space-y-3">
-          <h2 className="text-lg text-neutral-200">Scoring Capabilities</h2>
-          <Input
-            type="text"
-            id="scoring-capabilities"
-            name="scoring-capabilities"
-            placeholder="Enter scoring capabilities"
-          />
-        </section>
-      </form>
-    </Card>
+    <div className="space-y-6">
+      <section className="space-y-3">
+        <h2 className="text-lg text-neutral-200">Scoring Capabilities</h2>
+        <Input
+          type="text"
+          id="scoring-capabilities"
+          name="scoring-capabilities"
+          placeholder="Enter scoring capabilities"
+        />
+      </section>
+    </div>
   );
 }
--- apps/scouting/src/app/forms/pit/page.tsx
+++ apps/scouting/src/app/forms/pit/page.tsx
@@ -1,9 +1,26 @@
+import { Button } from "@repo/ui/components/button";
+import { Card } from "@repo/ui/components/card";
+import { useState } from "react";
 import { PitForm } from "./Pitform";
 import { ScoringCapabilities } from "./ScoringCapabilities";
+
+const PAGES = [
+  { label: "Basic Data", component: PitForm },
+  { label: "Scoring Capabilities", component: ScoringCapabilities },
+];
 export default function PitFormPage() {
+  const [currentPage, setCurrentPage] = useState(0);
+  const CurrentPageComponent = PAGES[currentPage].component;
+  const progressPercentage = ((CurrentPage + 1) / PAGES.length) * 100;
   return (
     <main className="container mx-auto max-w-5xl space-y-4 py-8">
       <h1 className="text-3xl tracking-tight "> Pit Form </h1>
+
+      <div className="spave-y-4">
+        <div className="mb-4 h-2 w-full rounded-full bg-neutral-200">
+          <span>{PAGES[currentPage].label}</span>
+        </div>
+      </div>
       <PitForm />
       <ScoringCapabilities />
     </main>
