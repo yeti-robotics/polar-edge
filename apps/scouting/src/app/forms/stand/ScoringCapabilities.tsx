c267c65787caa962c6135132224ad88bf0870952
Shivang Patel <shivangpatel2050@gmail.com>
2026-01-27 20:20:41 -0500

    created a basic layout and will add field

---
+++ apps/scouting/src/app/forms/stand/ScoringCapabilities.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@repo/ui/components/input";
+
+export function ScoringCapabilities() {
+  return (
+    <div className="space-y-6">
+      <section className="space-y-3">
+        <h2 className="text-lg text-neutral-200"> Data </h2>
+        <Input placeholder="This Will be filled out soon with more important dat" />
+      </section>
+    </div>
+  );
+}
---
+++ apps/scouting/src/app/forms/stand/StandForm.tsx
@@ -0,0 +1,12 @@
+export function StandForm() {
+  //going to use the Card component in page.tsx to wrap this form (got rid of it here, so that I can make it like 2 pages better)
+  return (
+    <div className="space-y-6">
+      <section className="space-y-4">
+        <h1>
+          Will Fill this Out after looking into the schemas after locking in on what is in the Wiki.
+        </h1>
+      </section>
+    </div>
+  );
+}
