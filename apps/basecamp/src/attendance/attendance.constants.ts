d92a5ac5fa5862d9de32d7956d7fa1f4b92fbfe5
Sam Perlmutter <sam.perlmutter227@gmail.com>
2026-02-07 11:44:23 -0500

    add attendance logic check

--- apps/basecamp/src/attendance/attendance.constants.ts
+++ apps/basecamp/src/attendance/attendance.constants.ts
@@ -1,4 +1,4 @@
-/** Time after which a "forgot to sign out" gets auto-resolved (3.5 hours) */
+/** Time after which a "forgot to sign out" gets auto-resolved (18 hours) */
 export const STALE_SIGNIN_THRESHOLD_MS = 1000 * 60 * 60 * 18;
 
 /** Default credit given when user forgot to sign out (1.5 hours) */
