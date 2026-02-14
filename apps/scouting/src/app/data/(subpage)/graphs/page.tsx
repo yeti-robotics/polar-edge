57096b51ab78ac5b21e1cbfd1cddd80660caefbf
AnirvinP <anirvin421potaraju@gmail.com>
2026-02-14 10:30:55 -0500

    graphs partially finished, need to add in database connection with fake data we have.

--- apps/scouting/src/app/data/(subpage)/graphs/page.tsx
+++ apps/scouting/src/app/data/(subpage)/graphs/page.tsx
@@ -1,68 +1,63 @@
-"use client";
-
 import { Card, CardContent } from "@repo/ui/components/card";
-import dynamic from "next/dynamic";
 import type { LineGraphProps } from "./LineGraphData";
+import LineGraphPerformanceCharts from "./LineGraphData";
 
-const LineGraphPerformanceCharts = dynamic(() => import("./LineGraphData"), {
-  ssr: false,
-  loading: () => <div className="h-80 animate-pulse bg-muted rounded-lg" />,
-});
+async function getGraphData(): Promise<LineGraphProps["data"]> {
+  return [
+    {
+      matchNumber: 1,
+      alliance: "red",
+      totalPoints: 56,
+      autoPoints: 12,
+      teleopPoints: 28,
+      climbPoints: 16,
+    },
+    {
+      matchNumber: 2,
+      alliance: "blue",
+      totalPoints: 52,
+      autoPoints: 10,
+      teleopPoints: 27,
+      climbPoints: 15,
+    },
+    {
+      matchNumber: 3,
+      alliance: "red",
+      totalPoints: 61,
+      autoPoints: 14,
+      teleopPoints: 30,
+      climbPoints: 17,
+    },
+    {
+      matchNumber: 4,
+      alliance: "blue",
+      totalPoints: 49,
+      autoPoints: 9,
+      teleopPoints: 25,
+      climbPoints: 15,
+    },
+    {
+      matchNumber: 5,
+      alliance: "red",
+      totalPoints: 64,
+      autoPoints: 15,
+      teleopPoints: 31,
+      climbPoints: 18,
+    },
+    {
+      matchNumber: 6,
+      alliance: "blue",
+      totalPoints: 58,
+      autoPoints: 11,
+      teleopPoints: 29,
+      climbPoints: 18,
+    },
+  ];
+}
 
-const sampleData: LineGraphProps["data"] = [
-  //here just to test the ui instantly for right now (ima get rid of it later when we connect to database)
-  {
-    matchNumber: 1,
-    alliance: "red",
-    totalPoints: 56,
-    autoPoints: 12,
-    teleopPoints: 28,
-    climbPoints: 16,
-  },
-  {
-    matchNumber: 2,
-    alliance: "blue",
-    totalPoints: 52,
-    autoPoints: 10,
-    teleopPoints: 27,
-    climbPoints: 15,
-  },
-  {
-    matchNumber: 3,
-    alliance: "red",
-    totalPoints: 61,
-    autoPoints: 14,
-    teleopPoints: 30,
-    climbPoints: 17,
-  },
-  {
-    matchNumber: 4,
-    alliance: "blue",
-    totalPoints: 49,
-    autoPoints: 9,
-    teleopPoints: 25,
-    climbPoints: 15,
-  },
-  {
-    matchNumber: 5,
-    alliance: "red",
-    totalPoints: 64,
-    autoPoints: 15,
-    teleopPoints: 31,
-    climbPoints: 18,
-  },
-  {
-    matchNumber: 6,
-    alliance: "blue",
-    totalPoints: 58,
-    autoPoints: 11,
-    teleopPoints: 29,
-    climbPoints: 18,
-  },
-];
-
-function GraphData() {
-  const dataSorted = [...sampleData].sort((a, b) => a.matchNumber - b.matchNumber);
+export default async function GraphDataPage() {
+  const data = await getGraphData();
+  const dataSorted = [...data].sort((a, b) => a.matchNumber - b.matchNumber);
   return (
     <div>
       <div className="container">
@@ -81,5 +76,3 @@ function GraphData() {
     </div>
   );
 }
-
-export default GraphData;
