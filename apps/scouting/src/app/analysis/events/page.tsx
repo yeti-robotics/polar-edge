e9f01ccae02c591332c2b93a859fa41a2abe54c3
AnirvinP <anirvin421potaraju@gmail.com>
2026-02-24 18:49:36 -0500

    feat(scouting): build EVENT overview page with sortable teams table new version of the event overview page, other PR had unecessary file changes.
    main goal is to complete issue: #363

--- apps/scouting/src/app/analysis/events/page.tsx
+++ apps/scouting/src/app/analysis/events/page.tsx
@@ -1,3 +1,36 @@
-export default function Events() {
-  return <div>Events page go here</div>;
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@repo/ui/components/breadcrumb";
+import { routes } from "@/lib/routes";
+
+function ComparisonPageBreadCrumbComponent() {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href={routes.analysis.root}>Analysis</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href={routes.analysis.comparison}>event-overview</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
+
+export default function ComparisonPage() {
+  return (
+    <div className="space-y-6">
+      <span>
+        <ComparisonPageBreadCrumbComponent />
+      </span>
+      <h1 className="text-2xl font-bold tracking-tight mt-4">Event Overview</h1>
+      <p className="text-muted-foreground">Skeleton for the event overview page here</p>
+    </div>
+  );
 }
