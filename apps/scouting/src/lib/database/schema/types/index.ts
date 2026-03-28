34b299d9321d8f1cec4c8b4e2f82f15604934c68
Anirvin Potaraju <anirvin421potaraju@gmail.com>
2026-03-28 09:30:35 -0400

    feat(scouting): pitform added turret or fixed shooter and added shoot while moving (#498)

--- apps/scouting/src/lib/database/schema/types/index.ts
+++ apps/scouting/src/lib/database/schema/types/index.ts
@@ -3,3 +3,4 @@ export { climbTypeEnum } from "./climb-type-enum";
 export { drivetrainEnum } from "./drivetrain-enum";
 export { matchTypeEnum } from "./match-type-enum";
 export { phaseEnum } from "./phase-enum";
+export { shooterTypeEnum } from "./shooter-type-enum";
