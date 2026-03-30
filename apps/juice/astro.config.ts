72e20ef6cfcee5ec95158e44388f9b8ff495e2cd
Drew Beamer <andrewmbeamer@gmail.com>
2026-03-30 18:46:07 -0400

    fix: change base

--- apps/juice/astro.config.ts
+++ apps/juice/astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/juice",
+  base: "/polar-edge/juice",
   integrations: [react()],
   vite: {
     css: {
