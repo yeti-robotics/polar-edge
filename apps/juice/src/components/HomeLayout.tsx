048fc8638003304548959fe25223a530e12620ab
Bhanu-Vanukuri <bvanukurib@gmail.com>
2026-05-14 21:38:06 -0400

    remove unnecessary HomeLayout component (kevin's suggestions)

--- apps/juice/src/components/HomeLayout.tsx
+++
@@ -1,7 +0,0 @@
-interface HomeLayoutProps {
-  children: React.ReactNode;
-}
-
-export function HomeLayout({ children }: HomeLayoutProps) {
-  return <div className="flex flex-col gap-6">{children}</div>;
-}
